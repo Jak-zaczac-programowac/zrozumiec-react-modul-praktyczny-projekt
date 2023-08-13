57202dda2bcae0671d8309918b0a7d5b6ae8a056
Kacper Sokołowski <kontakt@kaapa.pl>
2023-08-13 20:01:52 +0200

    Small screen fixes

--- e-commerce-store/src/components/SummerSale/SummerSale.jsx
+++ e-commerce-store/src/components/SummerSale/SummerSale.jsx
@@ -5,11 +5,11 @@ import styles from "./SummerSale.module.css";
 
 function SummerSale() {
     return (
-        <MaxWidthContainer>
-            <div
-                className={styles["summer-sale-cover-image"]}
-                style={{ backgroundImage: `url(${SummerSaleImage}` }}
-            >
+        <div
+            className={styles["summer-sale-cover-image"]}
+            style={{ backgroundImage: `url(${SummerSaleImage}` }}
+        >
+            <MaxWidthContainer>
                 <div className={styles["summer-sale-prompt"]}>
                     <div>
                         <h2>Letnie promocje do -70%!</h2>
@@ -18,8 +18,8 @@ function SummerSale() {
 
                     <Button>Sprawdź produkty</Button>
                 </div>
-            </div>
-        </MaxWidthContainer>
+            </MaxWidthContainer>
+        </div>
     );
 }
 
