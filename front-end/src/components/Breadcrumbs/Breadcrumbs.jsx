4fc022ecf7470344316d20ba2b60495aaa304126
Kacper Sokołowski <kontakt@kaapa.pl>
2023-09-09 13:41:30 +0200

    lekcja 10 done

--- front-end/src/components/Breadcrumbs/Breadcrumbs.jsx
+++ front-end/src/components/Breadcrumbs/Breadcrumbs.jsx
@@ -1,29 +1,45 @@
 import styles from "./Breadcrumbs.module.css";
 import { NavLink } from "react-router-dom";
 import ARROW_ICON from "../../assets/arrow.svg";
+import { useParams } from "react-router-dom";
+import { CATEGORIES, GENDERS } from "../../constants/categories";
 
 export function Breadcrumbs() {
+    const { gender, category, subcategory } = useParams();
+    const foundGender = GENDERS.find((g) => g.path === gender);
+    const foundCategory = CATEGORIES.find((c) => c.path === category);
+    console.log(foundCategory);
+
     const breadcrumbs = [
         {
-            categoryName: "Kobieta",
-            path: "kobieta",
+            categoryName: foundGender.categoryName,
+            path: `/${foundGender.path}`,
         },
         {
-            categoryName: "Odzież",
-            path: "odziez",
-        },
-        {
-            categoryName: "Swetry",
-            path: "swetry",
+            categoryName: foundCategory.categoryName,
+            path: `/${foundGender.path}/${foundCategory.path}`,
         },
     ];
+
+    if (subcategory) {
+        const foundSubCategory = foundCategory.subcategories.find(
+            (sc) => sc.path == subcategory
+        );
+
+        if (foundCategory) {
+            breadcrumbs.push({
+                categoryName: foundSubCategory.categoryName,
+                path: `/${foundGender.path}/${foundCategory.path}/${foundSubCategory.path}`,
+            });
+        }
+    }
 
     return (
         <ul className={styles.breadcrumbs}>
             {breadcrumbs.map((breadcrumb) => {
                 return (
                     <li key={breadcrumb.path}>
-                        <NavLink to={breadcrumb.path}>
+                        <NavLink to={breadcrumb.path} end>
                             {breadcrumb.categoryName}
                             <img src={ARROW_ICON} />
                         </NavLink>
