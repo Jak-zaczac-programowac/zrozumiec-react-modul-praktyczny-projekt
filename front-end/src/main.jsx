cbd330217407402c75808cdc16d33fbc8e61bc99
Kacper Sokołowski <kontakt@kaapa.pl>
2023-10-26 14:10:55 +0200

    Komponenty wspolne skonczone

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,12 +3,12 @@ import "./styles/globals.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Favourites } from "./views/Favourites/Favourites";
+import { Layout } from "./components/Layout/Layout";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Favourites />,
+        element: <Layout />,
     },
 ]);
 
